71c775bea67596f853e3b97aa32ed744612f040d
Kicherito <vanya.007@bk.ru>
2025-09-27 17:56:25 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from flask_sqlalchemy import SQLAlchemy
 import re
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='/root/parking/templates')
 app.secret_key = 'super_secret_key_12345'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=30)
 
@@ -466,5 +466,6 @@ def logout():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
+
 
     app.run(host='0.0.0.0', port=5000, debug=True)
